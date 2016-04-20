c836b1c3e2fefee9147765d7fbb9cdf4981fdbd9
tankerdream <ljy091@126.com>
2016-04-20 10:27:28 +0800

    +getPublicKey

---
+++ examples/getPublickey.js
@@ -0,0 +1,33 @@
+var Hyga = require('../');
+
+var config = {
+  uuid:'9c417707-420d-4ff1-95b5-dfe9171d5cba',
+  token:'52435eb0d78a761e1909793a6fdb15d4bf90570a'
+}
+
+var hyga = new Hyga(config);
+
+hyga.on('error',function(error){
+  console.error(error);
+});
+
+hyga.connect(function(response){
+
+  console.log('ready');
+
+  var targetDevice = {
+    uuid: '3e9fd243-2d75-42a4-89b9-a4e70a51b58d'
+  }
+
+  hyga.getPublicKey(targetDevice, function(success,res){
+    if(success){
+      console.log('Get PublicKey:');
+      console.log(res);
+    }else{
+      console.log('Error!');
+      console.log(res);
+    }
+    process.exit();
+  });
+
+});
--- examples/message.js
+++ examples/message.js
@@ -1,7 +1,7 @@
 var Hyga = require('../');
 
 var config = {
-  uuid:'c417707-420d-4ff1-95b5-dfe9171d5cba',
+  uuid:'9c417707-420d-4ff1-95b5-dfe9171d5cba',
   token:'52435eb0d78a761e1909793a6fdb15d4bf90570a'
 }
 
@@ -19,7 +19,7 @@ hyga.connect(function(response){
 
   // Message - response emits event 'message'
   var message = {
-    devices: 'e9fd243-2d75-42a4-89b9-a4e70a51b58d',
+    devices: '3e9fd243-2d75-42a4-89b9-a4e70a51b58d',
     payload: {ilove: 'food'},
     other: '林允儿'
   };
